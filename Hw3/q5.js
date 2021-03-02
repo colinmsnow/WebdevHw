87b9d41e80816d47f977090c1d5048c9b87ade7f
csnow <colinmsnow@gmail.com>
2021-03-02 17:37:15 -0500

    Code cleaned up and ready for tabs

--- Hw3/q5.js
+++ Hw3/q5.js
@@ -1,98 +1,24 @@
 /* Code for hw3 q5*/
 
 function makeEditableTable(id){
-
-
-    console.log("This is a test")
+    // Make all elements of the table editable on click
 
     let mytable = document.getElementById(id)
-
-    // mytable.addEventListener('click', table_clicked)
 
     for (var r = 0, n = mytable.rows.length; r < n; r++) {
         for (var c = 0, m = mytable.rows[r].cells.length; c < m; c++) {
-            element = mytable.rows[r].cells[c]
-            element.addEventListener('click', function() { table_clicked(this, id); } ) // calls function on clickedelement
-
+            // element = mytable.rows[r].cells[c]
+            mytable.rows[r].cells[c].addEventListener('click', function() { table_clicked(this, id); } ) // calls function on clickedelement
+            // mytable.rows[r].cells[c].addEventListener('click', function(){this.focus(); this.select()})
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-    // let tablerect = mytable.getBoundingClientRect()
-    // console.log(tablerect.top, tablerect.left, tablerect.bottom, tablerect.right)
-    // console.log(mytable.rows)
-    // let newdiv = document.createElement("div");
-    // document.body.appendChild(newdiv)
-    // newdiv.style.position = "absolute"
-    // newdiv.style.left = tablerect.left
-    // newdiv.style.top = tablerect.top
-    // newdiv.style.width = tablerect.right-tablerect.left
-    // newdiv.style.height = tablerect.bottom-tablerect.top
-    // newdiv.setAttribute('left', tablerect.left)
-    // newdiv.setAttribute('top', tablerect.top)
-
-    // for (let i=0, row; row = mytable.rows[i]; i++){
-
-    //     for (let j=0, col; col=row.cells[j]; j++)
-    //         var rect = col.getBoundingClientRect();
-    //         console.log(rect.top, rect.left, rect.bottom, rect.right)
-    //         let inputBox = document.createElement("input")
-    //         inputBox.setAttribute('type', 'text')
-    //         inputBox.setAttribute('value', 'default')
-    //         inputBox.setAttribute('position', 'absolute')
-    //         inputBox.setAttribute('left', rect.left)
-    //         inputBox.setAttribute('top', rect.top)
-
-    //         newdiv.appendChild(inputBox)
-           
-            
-    // }
-
-    // let element = null
-
-    // for (var r = 0, n = mytable.rows.length; r < n; r++) {
-    //     for (var c = 0, m = mytable.rows[r].cells.length; c < m; c++) {
-    //         element = mytable.rows[r].cells[c]
-    //         console.log(element.innerHTML);
-
-    //         var rect = element.getBoundingClientRect();
-    //         console.log(rect.top, rect.left, rect.bottom, rect.right)
-    //         let inputBox = document.createElement("input")
-    //         inputBox.setAttribute('type', 'text')
-    //         inputBox.setAttribute('value', mytable.rows[r].cells[c].innerHTML)
-    //         inputBox.style.position = "absolute"
-    //         inputBox.style.left =  rect.left
-    //         inputBox.style.top =  rect.top
-    //         inputBox.style.width = rect.right-rect.left
-    //         inputBox.style.height = rect.bottom-rect.top
-    //         // inputBox.setAttribute('width', rect.right-rect.left)
-
-    //         newdiv.appendChild(inputBox)
-    //     }
-    // }
-
-
-
-
-
 }
 
 function table_clicked(a, id){
+    // Find the clicked element, put a box on it, and set a listener
 
     let mytable = document.getElementById(id)
-    let tablerect = mytable.getBoundingClientRect()
     let elementrect = a.getBoundingClientRect()
-
 
     let mi = document.createElement("input")
     mi.type= "text"
@@ -106,16 +32,13 @@ function table_clicked(a, id){
     mi.addEventListener("blur", function(c){updatetable(c, mi, a)})
 
     mytable.appendChild(mi)
-
     console.log("clicked")
-    console.log(a)
 }
 
 function updatetable(c, inputElement, tableElement){
-    console.log(c)
+    // Save value to element and remove the input box
     tableElement.innerHTML = inputElement.value
     inputElement.parentNode.removeChild(inputElement);
-
 }
 
 window.onload = init
