ff242e50b1340faefbab6bc63c3e06022b23fa6a
csnow <colinmsnow@gmail.com>
2021-04-14 16:30:16 -0400

    React app loads from flaskgit add -Agit add -Agit add -A

--- FinalProject/Sockets/Frontend/frontend/src/App.js
+++ FinalProject/Sockets/Frontend/frontend/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component }  from 'react';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <p>My Token = {window.token}</p>
     </div>
   );
 }
