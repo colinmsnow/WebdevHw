fdd66549851976e00f207c0a44d2f83720a6294e
csnow <colinmsnow@gmail.com>
2021-04-14 16:02:37 -0400

    Setting up react app

---
+++ Hw4 copy/request.js
@@ -0,0 +1,16 @@
+function addPicture(input_url) {
+    // Makes a request to add a picture to the images folder
+
+    urlObject = {url:input_url}
+	const pAddPict = fetch('http://localhost:8080/new-picture-url', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(urlObject)
+        })
+}
+
+addPicture("https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg")
+
+// TODO: Do we need to add local file stuff here as well?
---
+++ Hw4 copy/server.py
@@ -0,0 +1,173 @@
+from flask import Flask, jsonify, send_from_directory, request, flash, redirect
+from werkzeug.utils import secure_filename
+import uuid
+import datetime
+import copy
+import requests
+import pickle
+import os.path
+
+
+app = Flask(__name__)
+
+PORT = 8080
+IMAGES_FOLDER = 'image'
+PICTURES = {} # This is where the data is stored.
+# Its a gross global dictionary but jsonify likes dicts so its easier this way
+
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg'])
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def create_picture(source):
+    """ Creates a picture object which stores a unique id, timestamp, comments, and the image source as a filename """
+    return {"id": str(uuid.uuid1()), "timestamp": datetime.datetime.now().isoformat(), "source": source, "comments": []}
+
+
+def add_image(images, source):
+    """Adds an image to an existing image dict.
+    Assigns a unique id which it can be accessed by
+
+    ex. 
+
+    {
+        "<random id>" : {"id":<id>, "timestamp": <timestamp>, "source":, <source>, "comments": ,comments>},
+        "<another random id>" : {"id":<id>, "timestamp": <timestamp>, "source":, <source>, "comments": ,comments>}
+    }
+    """
+    pic = create_picture(source)
+    images[pic["id"]] = pic
+
+    pickle.dump( images, open( "pictures.p", "wb" ) )
+    return (images, pic["id"])
+
+def save_data():
+
+    """ Update the persistent stuff in the backend by re downloading the new version """
+
+    pickle.dump( PICTURES, open( "pictures.p", "wb" ) )
+
+
+
+#def add_image(file_path):
+    
+
+# we need this or else it shuts down post requests
+@app.after_request
+def add_header(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = '*'
+    return response     
+
+
+# Test route
+@app.route('/test')
+def test_route():
+    # this will be interpreted by a browser as a (very) broken-down HTML doc
+    return "Hello world"
+
+
+@app.route('/pictures') # works
+def get_pictures():
+    """ Fetches the list of pictures without the comments or source """
+    filteredPics = copy.deepcopy(PICTURES)
+    print(filteredPics)
+    for i in filteredPics.values():
+        i.pop("source")
+        i["comments"] = len(i["comments"])
+    result = {'pictures': filteredPics}
+    return jsonify(result)
+
+
+@app.route('/picture/<ID>') # works
+def get_picture(ID):
+    """ Fetches a specific picture with all of its comments and its source """
+    pic = PICTURES[ID]
+    print("Pic is: " + str(pic))
+    filename = pic["source"]
+
+    result = {'picture': pic}
+    return jsonify(result)
+
+
+@app.route('/new-picture-url', methods=['POST']) # works
+def add_picture():
+    """ Adds a picture to the picture folder """
+
+    global PICTURES
+
+    print("GOT A REQUEST")
+    
+    data = request.get_json()
+    print("DATA: " + str(data))
+    link = data
+    print("LINK: " + str(link))
+
+    picID = str(uuid.uuid1()) + ".jpg"
+    print(picID)
+
+    PICTURES, new_id = add_image(PICTURES, picID)
+    r = requests.get(link)
+    print ("PICTURES: " + str(PICTURES))
+
+    with open(IMAGES_FOLDER + "/" + picID, 'wb') as f:
+        f.write(r.content)
+
+    return_data = {"id": new_id, "timestamp": PICTURES[new_id]["timestamp"]}
+
+    return jsonify(return_data)
+
+@app.route('/new-picture-upload', methods=['POST'])
+def upload_file():
+
+    global PICTURES
+
+    print("GOT A REQUEST")
+    
+    file = request.files['new-file']
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(app.config[IMAGES_FOLDER], filename))
+    flash('File successfully uploaded')
+    return redirect('/')    
+
+@app.route('/comments/<ID>') # get route #works
+def get_comments(ID):
+    pic = PICTURES[ID]
+    comments = pic["comments"]
+    result = {"comments": comments}
+    return jsonify(result)
+
+
+@app.route('/new-comment/<ID>', methods=['POST']) # post route #works
+def new_comment(ID):
+    ''' adds a comment to a picture '''
+
+    print("GOT A COMMENT REQUEST")
+    print(request.get_json())
+    print(type(request.get_json()))
+    data = request.get_json()
+    global PICTURES
+    pic = PICTURES[ID]
+    comments = pic["comments"]
+    comments.append({"comment": data['comment'], "timestamp": PICTURES[ID]["timestamp"]})
+    save_data()
+    result = {"comments": comments}
+    return jsonify(result)
+
+
+@app.route('/<path:path>')
+def catch_all(path):
+    print("PATH IS: " + str(path))
+    return send_from_directory("", path)
+
+
+
+if os.path.exists("pictures.p"):
+    PICTURES = pickle.load(open("pictures.p", "rb" ))
+else:
+    PICTURES, _ = add_image(PICTURES, "Starsinthesky.jpg")
+    PICTURES, _ = add_image(PICTURES, "Starsinthesky.jpg")
+print(PICTURES)
+app.run(port=PORT)
