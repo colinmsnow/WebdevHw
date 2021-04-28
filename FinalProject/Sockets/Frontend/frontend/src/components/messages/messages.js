c733598cd2400f4fee472e87dc30c0d99e86eb96
Maia Materman <maiam6242@gmail.com>
2021-04-27 20:06:40 -0400

    :bug:

--- FinalProject/Sockets/Frontend/frontend/src/components/messages/messages.js
+++ FinalProject/Sockets/Frontend/frontend/src/components/messages/messages.js
@@ -1,15 +1,19 @@
 import React  from "react";
 import './messages.css'
 
-function showTime(bool, e) {
-    if (bool){
-       
-    }
-    else {
-       
-    }
+// function showTime(bool, time, e) {
+//     if (bool){
+//         return (time)
+        
+//         console.log(time)
+//     }
+//     else {
+//        //TODO: Fix Me!
+//     }
   
-}
+// }
+// {(e) => showTime(true, props.time, e)} 
+let showTime;
 
 function Purple_Message(props) {
     return <div className= "purple message">
@@ -18,12 +22,15 @@ function Purple_Message(props) {
 }
 
 function Pink_Message(props) {
-    return <div className= "pink message" onMouseEnter={(e) => showTime(true, e)} onMouseLeave={(e) => showTime(false, e)} 
-    >
+    return <div className= "pink message" onMouseEnter={showTime = true} onMouseLeave={showTime = false} 
+    > 
+        {showTime && 
+        <div className="time_label">
+            {props.time}
+        </div>}     
         <p>{props.message}</p>
     </div>
 }
-
 
 
 export {Purple_Message, Pink_Message};
